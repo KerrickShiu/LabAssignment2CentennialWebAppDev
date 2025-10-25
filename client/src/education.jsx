2898bc690af9eac4a386542f9f775acb74f6c5f0
KerrickShiu <kshiu8@my.centennialcollege.ca>
2025-10-25 18:10:41 -0400

    first commit

---
+++ client/src/education.jsx
@@ -0,0 +1,9 @@
+export default function Education() {
+return (
+<div>
+    <h2>Education</h2>
+    <li>Sir John A. Macdonald CI OSSD 2022</li>
+    <li>Centennial College Health Informatics Technology Advanced Diploma 2025-Present</li>
+    </div>
+);
+}
